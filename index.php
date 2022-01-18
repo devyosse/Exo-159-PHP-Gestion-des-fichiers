a205725c4cd27f86a9997561c010653ebd113a7d
devyosse <narutoshippuden884@gmail.com>
2022-01-18 14:02:48 +0100

    exo 159

--- index.php
+++ index.php
@@ -6,21 +6,35 @@
  */
 // TODO Votre code ici.
 
+$contents = file_get_contents('lire.txt');
+if ($contents !== false) {
+    echo $contents;
+}
+
 /**
- * 3. Faites la même chose à l'aide d'une boucle et de la fonction fgets(), récupérez les lignes du fichier une à une.
- * 4. Affichez le contenu de chaque ligne dans un nouveau span.
- * --> N'oubliez pas de fermer votre fichier.
- */
+* 3. Faites la même chose à l'aide d'une boucle et de la fonction fgets(), récupérez les lignes du fichier une à une.
+* 4. Affichez le contenu de chaque ligne dans un nouveau span.
+* --> N'oubliez pas de fermer votre fichier.
+*/
 // TODO Votre code ici.
-
+$file = fopen('lire.txt', 'rb');
+if ($file !== false){
+    echo fgets($file);
+}
 
 /**
- * 5. Récupérez les caractères un à un et n'affichez le caractère que s'il s'agit d'autre chose qu'un espace, qu'un point ou qu'un point d'interrogation / exclamation.
- * --> N'oubliez pas de fermer votre fichier.
- */
+* 5. Récupérez les caractères un à un et n'affichez le caractère que s'il s'agit d'autre chose qu'un espace, qu'un point ou qu'un point d'interrogation / exclamation.
+* --> N'oubliez pas de fermer votre fichier.
+*/
 // TODO Votre code ici.
+$fil = fopen('lire.txt', 'rb');
 
+$first = fgetc($fil);
+echo $first;
+$second = fgetc($fil);
+echo $second;
 
 /**
- * 6. Ne cherchez pas la logique dans cet exo ^^
- */
+* 6. Ne cherchez pas la logique dans cet exo ^^
+*/
+
